90cf7ae4e2cbbbe37e16600f4d341d17b3bffc3d
AitorHernandez1 <64251085+AitorHernandez1@users.noreply.github.com>
2023-03-15 23:13:39 +0100

    Update automatizacion.py

--- src/automatizacion.py
+++ src/automatizacion.py
@@ -1,3 +1,15 @@
+import gspread
+import time
+import pandas as pd
+import os
+import requests
+import os
+import requests
+import glob
+from Levenshtein import distance
+from dotenv import load_dotenv
+from oauth2client.service_account import ServiceAccountCredentials
+
 creds = ServiceAccountCredentials.from_json_keyfile_name('gspread/service_account.json')
 client = gspread.authorize(creds)
 # open second shee
