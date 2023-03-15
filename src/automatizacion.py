637929b98e960093bf0dfe3c49d8e8ecb895ab9f
AitorHernandez1 <64251085+AitorHernandez1@users.noreply.github.com>
2023-03-15 22:04:15 +0100

    Add files via upload

--- src/automatizacion.py
+++ src/automatizacion.py
@@ -1,17 +1,3 @@
-
-# !pip install python-Levenshtein
-
-import pandas as pd
-import numpy as np
-
-import os
-import glob
-import time
-import requests
-from sys import platform
-
-from Levenshtein import distance
-
 import gspread
 import time
 import pandas as pd
@@ -20,18 +6,16 @@ import requests
 import os
 import requests
 import glob
+from Levenshtein import distance
 from dotenv import load_dotenv
 from oauth2client.service_account import ServiceAccountCredentials
-creds = ServiceAccountCredentials.from_json_keyfile_name('service_account.json')
+creds = ServiceAccountCredentials.from_json_keyfile_name('gspread/service_account.json')
 client = gspread.authorize(creds)
 # open second shee
 sheet = client.open('Users TUDUU').worksheet('reecomendaciones')
 client = gspread.authorize(creds)
 
-from probabilidades import *
-from funciones import *
-
-sa = gspread.service_account(filename = "./../gspread/service_account.json")
+sa = gspread.service_account()
 sh = sa.open('Users TUDUU')
 wks = sh.worksheet('automatizacion')
 wks2 = sh.worksheet('productos')
@@ -61,40 +45,108 @@ while True:
         print('hola')
         load_dotenv()
         print('holaa')
-        path = '../cluster_2_1/' # use your path
+        path = r'cluster_2_1/' # use your path
         #save into df all files
         all_files = glob.glob(path + "/*.xlsx")
-        print('All files')
 
         li = dict()
         for filename in all_files:
             df = pd.read_excel(filename, index_col=None, header=0)
-            print('ok')
-            filename = filename.split('/')[1].split('.')[0]
-            print('split hecho')
-            # if platform =='darwin':
-            #     filename = filename.split('/')[1].split('.')[0] # mac
-            # elif platform == 'win32':
-            #     filename = filename.split('\\')[1].split('.')[0] # windows
+            filename = filename.split('\\')[1].split('.')[0]
             df['prob'] = 1/len(df)
             li[filename] = df
-            print(filename)
-            print('filename')
 
-        # Segmento de cliente
-        tipo_comprador1 = '1' # poner el segmento real del cliente
-        
-        # print(tipo_comprador1)
+
+
+        def is_in_list(a, b):
+            a = a.split(' ')
+            a = [word.lower() for word in a]
+            return any(i in a for i in b)
+
+
+        def set_probabilities(tipo_cluster):
+            for i in range(len(li['Desayuno'] )):
+                if is_in_list(li['Desayuno'][' Name'][i], tipo_cluster[0]):
+                    li['Desayuno']['prob'][i] *= 100
+            for i in range(len(li['comida_cena'] )):
+                if is_in_list(li['comida_cena'][' Name'][i], tipo_cluster[1]):
+                    li['comida_cena']['prob'][i] *= 100
+            for i in range(len(li['entre_horas'] )):
+                if is_in_list(li['entre_horas'][' Name'][i], tipo_cluster[2]):
+                    li['entre_horas']['prob'][i] *= 100
+            Desayuno = li['Desayuno']
+            comida_cena = li['comida_cena']
+            entre_horas = li['entre_horas']
+            Cuidado_personal = li['Cuidado_personal']
+            Hogar = li['Hogar']
+            return Desayuno, comida_cena, entre_horas, Cuidado_personal, Hogar
+
+
+        print('holaa')
+            
+        probx3_2_1_desayuno = ['cereales', 'pan', 'tostada', 'palitos', 'baguette', 'hogaza', 'baguettes', 'panadería', 'cereales','aceite',  'tostadas','galletas','tortitas', 'leche', 'cerezas', 'manzana', 'plátano', 'fresa', 'higos', 'paraguayo', 'nectarina','manzana', 'aguacates', 'mango', 'aguacate',  'limón', 'melón', 'sandía', 'kiwis', 'bífidus', 'queso', 'yogur', 'mantequilla']
+        probx3_2_1_comida_cena = ['huevo','huevos','tortiglioni','helice','tallarín','cannelloni','fusilli','pasta', 'macarrón', 'tallarines', 'garbanzos', 'lentejas', 'alubias','albóndigas','entrecot','burguer','solomillo','salchichas','bacalao','mejillones','almejas', 'bonito','langostino','atún', 'rodaballo', 'rape', 'merluza','gallo', 'dorada', 'lubina','lenguado', 'calamar','sepia','gambón','filete','cebolla', 'ajo','zanahoria', 'albahaca', 'calabaza','salmorejo','sopa', 'gazpacho', 'patatas', 'patata', 'arroz', 'tomate', 'pollo,','proteínas', 'pollo', 'salmón','arroz','tiramisú','mousse','flan', 'helado','ensalada','champiñones','pimientos', 'espinacas','cebolla','lechuga', 'iceberg','zanahoria', 'brócoli', 'berenjena','berenjenas', 'coliflor', 'pepino','alcachofa','tomate', 'puerro', 'calabaza', 'calabacín','pimiento','patata', 'remolacha', 'zanahoria', 'cebolla', 'ajo','ajos','puerro','espárrago','canónigos', 'coliflor','champiñón', 'seta','zanahorias', 'tomates']
+        probx3_2_1_entre_horas = [ 'chocolate', 'galletas','cheesecake', 'copa','brownie','cacao', 'choco', 'berlina', 'gofres', 'sobaos', 'croissant','snack','chocolate', 'pizza', 'tarta', 'hamburguesa','mayonesa', 'nata', 'paté','nachos','york','quesos','morcilla', 'ibéricos', 'ibérico','jamón', 'chorizo','paletilla', 'ibérica', 'salchichón', 'longaniza','paleta','lonchas', 'pavo', 'fiambre','lonchas']
+        cluster1 = [probx3_2_1_desayuno, probx3_2_1_comida_cena, probx3_2_1_entre_horas]
+
+        prob_3_2_2_desayuno = ['cereales','pan', 'tostada', 'palitos', 'baguette','hogaza','baguettes', 'panadería','cereales','aceite',  'tostadas','galletas','leche','cerezas','albaricoque', 'manzana', 'plátano', 'fresa','uvas','ciruela', 'higos', 'paraguayo','naranja', 'mandarina', 'uva', 'peras', 'pera', 'piña','manzanas', 'manzana', 'banana', 'ciruelas', 'paraguayos', 'frambuesas', 'aguacates', 'mango', 'aguacate','limón', 'melón', 'sandía', 'kiwis','bífidus','yogur','cereales', 'pan', 'tostada', 'palitos', 'baguette', 'hogaza', 'baguettes', 'panadería', 'cereales','aceite',  'tostadas','galletas','tortitas', 'leche', 'cerezas', 'manzana', 'plátano', 'fresa', 'higos', 'paraguayo', 'nectarina','manzana', 'aguacates', 'mango', 'aguacate',  'limón', 'melón', 'sandía', 'kiwis', 'bífidus', 'queso', 'yogur', 'mantequilla','zumo','leche']
+        prob_3_2_2_comida_cena = ['huevo','huevos','tortiglioni','helice','tallarín','cannelloni','fusilli','pasta', 'spaghetti','macarrón', 'tallarines','garbanzo','legumbre', 'garbanzos', 'lentejas', 'alubias', 'lenteja', 'alubia', 'lomo', 'cerdo', 'pollo', 'pechuga','solomillo', 'bonito', 'atún', 'rodaballo', 'rape', 'merluza','gallo', 'dorada', 'lubina','lenguado', 'calamar','filete','cebolla', 'ajo', 'espinaca', 'alcachofa', 'zanahoria', 'albahaca', 'calabaza', 'remolacha', 'salmorejo','sopa', 'gazpacho','patatas', 'patata', 'arroz', 'tomate','pollo,','proteínas', 'pollo', 'salmón','arroz','tiramisú','mousse', 'flan', 'helado','ensalada','alcachofas','champiñones','alcachofa','verduras','verdura', 'pimientos', 'espinacas', 'cebolla','haba', 'rollitos', 'rodajas', 'lechuga', 'iceberg', 'zanahoria', 'brócoli', 'berenjena','judía','berenjenas', 'coliflor', 'pepino', 'escarola', 'alcachofa', 'tomate', 'puerro', 'calabaza', 'calabacín', 'pimiento', 'patata', 'remolacha', 'zanahoria', 'cebolla', 'ajo','ajos','puerro', 'maíz','remolacha', 'espárrago', 'puerro', 'espinaca','rúcula', 'lechuga', 'canónigos', 'coliflor', 'brócoli', 'alcachofa','acelgas','champiñón', 'seta', 'zanahorias', 'tomates', 'cebollas', 'batata', 'lechugas']
+        prob_3_2_2_entre_horas =  ['chocolate', 'galletas','cheesecake', 'copa','brownie','cacao', 'choco', 'berlina', 'trenza', 'napolitana', 'palmeritas', 'mantecadas','gofres', 'sobaos','croissant','snack','chocolate', 'pizza', 'tarta', 'hamburguesa','mayonesa', 'nata','paté','nachos','york','quesos','morcilla', 'ibéricos', 'ibérico','jamón', 'chorizo','chistorra', 'paletilla', 'ibérica', 'salchichón', 'longaniza','paleta','lonchas', 'pavo', 'fiambre','lonchas']
+        cluster2 = [prob_3_2_2_desayuno, prob_3_2_2_comida_cena, prob_3_2_2_entre_horas]
+
+        prob_3_2_3_desayuno = ['cereales','pan', 'tostada', 'palitos','baguette','hogaza', 'baguettes', 'panadería','cereales','aceite',  'tostadas','leche','cerezas','albaricoque','manzana', 'plátano', 'fresa','uvas','ciruela', 'higos', 'paraguayo', 'arándanos', 'nectarina', 'melocotón', 'naranja', 'mandarina', 'uva', 'peras', 'pera', 'piña','manzanas', 'manzana', 'banana', 'ciruelas', 'paraguayos', 'frambuesas', 'aguacates', 'mango', 'aguacate', 'limón', 'melón', 'sandía', 'kiwis','bífidus','yogur','cereales', 'pan', 'tostada', 'palitos', 'baguette', 'hogaza', 'baguettes', 'panadería', 'cereales','aceite',  'tostadas','galletas','tortitas', 'leche', 'cerezas', 'manzana', 'plátano', 'fresa', 'higos', 'paraguayo', 'nectarina','manzana', 'aguacates', 'mango', 'aguacate',  'limón', 'melón', 'sandía', 'kiwis', 'bífidus', 'queso', 'yogur', 'zumo','leche']
+        prob_3_2_3_comida_cena = ['huevo','huevos','tortiglioni','helice','tallarín','cannelloni','fusilli','pasta', 'spaghetti', 'macarrón', 'tallarines', 'garbanzo','legumbre', 'garbanzos', 'lentejas', 'alubias', 'lenteja', 'alubia','pollo','pechuga','bonito', 'atún', 'rodaballo', 'rape', 'merluza','gallo', 'dorada', 'lubina', 'lenguado', 'calamar','filete','cebolla', 'ajo', 'espinaca', 'alcachofa', 'zanahoria', 'albahaca', 'calabaza', 'remolacha','salmorejo','sopa', 'gazpacho', 'patatas', 'patata', 'arroz', 'tomate','pollo,','proteínas', 'pollo', 'integrales','tiramisú','mousse', 'flan', 'helado','ensalada','alcachofas','champiñones','alcachofa','verduras','verdura', 'pimientos', 'espinacas', 'cebolla','haba', 'rollitos', 'rodajas', 'lechuga', 'iceberg','zanahoria', 'brócoli', 'berenjena','judía','berenjenas', 'coliflor', 'pepino', 'escarola', 'alcachofa', 'tomate', 'puerro', 'calabaza', 'calabacín','pimiento','patata', 'remolacha', 'zanahoria', 'cebolla', 'ajo','ajos','puerro','maíz','remolacha','espárrago', 'puerro','espinaca','rúcula', 'lechuga', 'canónigos', 'coliflor', 'brócoli', 'alcachofa','acelgas','champiñón', 'seta','zanahorias', 'tomates', 'cebollas', 'batata', 'lechugas']
+        prob_3_2_3_entre_horas = ['york','quesos','ibéricos', 'ibérico','jamón','paletilla', 'ibérica','paleta', 'lonchas', 'pavo','lonchas']
+        cluster3 = [prob_3_2_3_desayuno, prob_3_2_3_comida_cena, prob_3_2_3_entre_horas]
+
+        prob_3_2_4_desayuno = ['cereales','pan', 'tostada', 'palitos','baguette','hogaza','baguettes', 'panadería','cereales','aceite',  'tostadas', 'leche','muesli','cerezas','albaricoque','manzana', 'plátano', 'fresa','uvas','ciruela', 'higos', 'paraguayo', 'arándanos', 'nectarina', 'melocotón', 'naranja', 'mandarina', 'uva', 'peras', 'pera', 'piña', 'manzanas', 'manzana', 'banana', 'ciruelas', 'paraguayos', 'frambuesas','aguacates', 'mango', 'aguacate', 'limón', 'melón', 'sandía', 'kiwis','bífidus','yogur','kéfir','cereales', 'pan', 'tostada', 'palitos', 'baguette', 'hogaza', 'baguettes', 'panadería', 'cereales','aceite',  'tostadas','galletas','tortitas', 'leche', 'cerezas', 'manzana', 'plátano', 'fresa', 'higos', 'paraguayo', 'nectarina','manzana', 'aguacates', 'mango', 'aguacate',  'limón', 'melón', 'sandía', 'kiwis', 'bífidus', 'queso', 'yogur','zumo','leche']
+        prob_3_2_4_comida_cena = ['huevo','huevos','tortiglioni','helice','tallarín','cannelloni','fusilli','pasta', 'spaghetti', 'macarrón', 'tallarines','garbanzo','legumbre', 'garbanzos', 'lentejas', 'alubias', 'lenteja', 'alubia','pollo','pechuga','bonito', 'atún', 'rodaballo', 'rape', 'merluza','gallo', 'dorada', 'lubina', 'lenguado', 'calamar','filete','cebolla', 'ajo', 'espinaca', 'alcachofa', 'zanahoria', 'albahaca', 'calabaza', 'remolacha','salmorejo','sopa', 'gazpacho','patatas', 'patata', 'arroz', 'tomate', 'mozzarella','pollo','proteínas', 'pollo', 'salmón', 'desnatada',  'desnatada', 'desnatada','avena','arroz','0%','integrales','avena','ensalada','alcachofas','champiñones','alcachofa','verduras','verdura', 'pimientos', 'espinacas', 'cebolla','haba', 'rollitos', 'rodajas', 'lechuga', 'iceberg','zanahoria', 'brócoli', 'berenjena','judía','berenjenas', 'coliflor', 'pepino', 'escarola', 'alcachofa','tomate', 'puerro', 'calabaza', 'calabacín','pimiento','patata', 'remolacha', 'zanahoria', 'cebolla', 'ajo','ajos','puerro','maíz','espárrago','puerro','espinaca','rúcula', 'lechuga', 'canónigos', 'coliflor', 'brócoli', 'alcachofa','acelgas','champiñón', 'seta', 'zanahorias', 'tomates', 'cebollas', 'batata', 'lechugas']
+        prob_3_2_4_entre_horas = ['york','lonchas', 'lonchas''lonchas','pavo', 'pavo','pavo','lonchas']
+        cluster4 = [prob_3_2_4_desayuno, prob_3_2_4_comida_cena, prob_3_2_4_entre_horas]
+
+        lista_carne = ["Albóndigas", "Entrecot", "Burguer", "Solomillo", "Salchichas", "Filete", "Pollo", "Ibéricos", "Ibérico", "Jamón", "Chorizo", "Paletilla", "Ibérica", "Salchichón", "Longaniza", "Paleta", "Pavo", "Fiambre", "Lonchas"]
+        lista_pescado = ["Bacalao", "Mejillones", "Almejas", "Bonito", "Langostino", "Atún", "Rodaballo", "Rape", "Merluza", "Gallo", "Dorada", "Lubina", "Lenguado", "Calamar", "Sepia", "Gambón", "Salmón"]
+        lista_veganos = ["Queso", "Leche", "Nata", "Mantequilla", "Yogur", "Huevos", "Miel"]
+
+        def modelo_recomendacion(tipo_comprador):
+            
+            if tipo_comprador == '1':
+                productos = [9,9,3,5,6]
+                Desayuno, comida_cena, entre_horas, Cuidado_personal, Hogar = set_probabilities(cluster1)
+                df_cesta = Desayuno.sample(n=productos[0], weights = Desayuno.prob).append(comida_cena.sample(n=productos[1], weights = comida_cena.prob)).append(entre_horas.sample(n=productos[2], weights = entre_horas.prob))#.append(Hogar.sample(n=productos[3])).append(Cuidado_personal.sample(n=productos[4]))
+                lista_productos = df_cesta[' Name'].tolist()
+                precio_total = df_cesta['Price'].sum()
+            elif tipo_comprador == '2':
+                productos = [9,9,7,5,6]
+                Desayuno, comida_cena, entre_horas, Cuidado_personal, Hogar = set_probabilities(cluster2)
+                df_cesta = Desayuno.sample(n=productos[0], weights = Desayuno.prob).append(comida_cena.sample(n=productos[1], weights = comida_cena.prob)).append(entre_horas.sample(n=productos[2], weights = entre_horas.prob))#.append(Hogar.sample(n=productos[3])).append(Cuidado_personal.sample(n=productos[4]))
+                lista_productos = df_cesta[' Name'].tolist()
+                precio_total = df_cesta['Price'].sum()
+            elif tipo_comprador == '3':
+                productos = [9,9,7,5,6]
+                Desayuno, comida_cena, entre_horas, Cuidado_personal, Hogar = set_probabilities(cluster3)
+                df_cesta = Desayuno.sample(n=productos[0], weights = Desayuno.prob).append(comida_cena.sample(n=productos[1], weights = comida_cena.prob)).append(entre_horas.sample(n=productos[2], weights = entre_horas.prob))#.append(Hogar.sample(n=productos[3])).append(Cuidado_personal.sample(n=productos[4]))
+                lista_productos = df_cesta[' Name'].tolist()
+                precio_total = df_cesta['Price'].sum()
+            elif tipo_comprador == '4':
+                productos = [9,9,7,5,6]
+                Desayuno, comida_cena, entre_horas, Cuidado_personal, Hogar = set_probabilities(cluster4)
+                df_cesta = Desayuno.sample(n=productos[0], weights = Desayuno.prob).append(comida_cena.sample(n=productos[1], weights = comida_cena.prob)).append(entre_horas.sample(n=productos[2], weights = entre_horas.prob))#.append(Hogar.sample(n=productos[3])).append(Cuidado_personal.sample(n=productos[4]))
+                lista_productos = df_cesta[' Name'].tolist()
+                precio_total = df_cesta['Price'].sum()
+            return lista_productos, precio_total
+
+
+        print('holaaaaa')
+        tipo_comprador1 = '1'
         while True:
-            cesta = modelo_recomendacion(tipo_comprador1, li)
+            cesta = modelo_recomendacion(tipo_comprador1)
             if cesta[1] > 50:
-                # print(len(cesta[1]))
                 break
         print('final')
 
+        df3 = pd.read_csv('Menu_Items.csv')
 
-        # Cesta recomendada
-        df3 = pd.read_csv('../Menu_Items.csv')
 
         #### CELIACOS
         
@@ -115,21 +167,24 @@ while True:
                 except:
                     pass
 
+        df3 = df3[df3[' Name'].isin(cesta[0])]
+        df3 = df3.groupby(' Name').apply(lambda x: x.sample(1))
+        print(df3[' Name'].tolist())
+
+
         ## Vegetarianismo
         print(df2['vegetariano'].iloc[-1])
 
         max_distance = 2
         if len(df2['vegetariano'].iloc[-1]) > 0:
             try:
-                productos_list = cesta
+                productos_list = df3[' Name']
                 productos_vegetarianos = [producto for producto in productos_list 
                                         if all(
                                                 distance(x.lower(), prod.lower())>max_distance
                                                 for prod in lista_carne+lista_pescado
                                                 for x in producto.split()
                                             )]
-                df3[' Name'] = productos_vegetarianos
-
             except:
                 pass
 
@@ -140,21 +195,15 @@ while True:
         max_distance=2
         if len(df2['vegano'].iloc[-1]) > 0:
             try:
-                # productos_list = df3[' Name']
-                productos_list = cesta
+                productos_list = df3[' Name']
                 productos_veganos = [producto for producto in productos_list 
                                         if all(
                                                 distance(x.lower(), prod.lower())>max_distance
                                                 for prod in lista_pescado+lista_carne+lista_veganos
                                                 for x in producto.split()
                                             )]
-                df3[' Name'] = productos_veganos
             except:
                 pass
-
-        df3 = df3[df3[' Name'].isin(cesta[0])]
-        df3 = df3.groupby(' Name').apply(lambda x: x.sample(1))
-        print(df3[' Name'].tolist())
         
 
         #print(cesta[0], cesta[1])
@@ -162,7 +211,7 @@ while True:
         df = pd.DataFrame(wks.get_all_records())
         
         sheet.append_row(df3[' Name'].tolist())
-                    
-    time.sleep(5) # Esti tiene que ser cada vez que detecte un cambio, no cada 5 segundos
+        print('final')
+    time.sleep(5)
 
     
