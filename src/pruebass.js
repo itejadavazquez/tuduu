cd146d5b6b0ddd889d32430c39e1f677da544903
Ane Martínez Orellana <64247896+anemartinez1@users.noreply.github.com>
2023-03-22 21:00:12 +0100

    Complicamos las pruebas

--- src/pruebass.js
+++ src/pruebass.js
@@ -1,1 +1,13 @@
 console.log("Hola");
+
+function ejemploLodash() {
+  // Cargar la librería Lodash
+  var response = UrlFetchApp.fetch('https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js');
+  eval(response.getContentText());
+
+  // Utilizar la librería Lodash
+  var array = [1, 2, 3, 4, 5];
+  var sum = _.sum(array);
+  Logger.log('La suma del array es ' + sum);
+}
+
