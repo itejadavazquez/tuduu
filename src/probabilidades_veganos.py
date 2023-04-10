64531ec605293e95211db9e809a1c97669a42462
Ane Martínez Orellana <ane.martinez@alumni.mondragon.edu>
2023-04-10 23:33:54 +0200

    Añadimos segmentación de clientes para TFG Aitor

---
+++ src/probabilidades_veganos.py
@@ -0,0 +1,77 @@
+# CLUSTER 1
+probx3_2_1_desayuno = ['cereales', 'pan', 'tostada', 'palitos', 'baguette', 'hogaza', 'baguettes', 'panadería', 'cereales', 'aceite', 
+                       'tostadas', 'galletas', 'tortitas', 'cerezas', 'manzana', 'plátano', 'fresa', 'higos', 'paraguayo',
+                        'nectarina', 'manzana', 'aguacates', 'mango', 'aguacate', 'limón', 'melón', 'sandía', 'kiwis', 'bífidus']
+
+probx3_2_1_comida_cena = ['tortiglioni', 'helice', 'tallarín', 'cannelloni', 'fusilli', 'pasta', 'macarrón', 'tallarines',
+                          'garbanzos', 'lentejas', 'alubias', 'cebolla', 'ajo', 'zanahoria', 'albahaca', 'calabaza', 'salmorejo',
+                          'sopa', 'gazpacho', 'patatas', 'patata', 'arroz', 'tomate', 'ensalada', 'champiñones', 'pimientos',
+                          'espinacas', 'lechuga', 'iceberg', 'brócoli', 'berenjena', 'berenjenas', 'coliflor', 'pepino', 'alcachofa',
+                          'tomate', 'puerro', 'calabaza', 'calabacín', 'pimiento', 'patata', 'remolacha', 'zanahoria', 'cebolla',
+                          'ajos', 'puerro', 'espárrago', 'canónigos', 'coliflor', 'champiñón', 'seta', 'zanahorias', 'tomates']
+
+probx3_2_1_entre_horas = ['chocolate', 'galletas', 'cacao', 'choco', 'berlina', 'gofres', 'sobaos', 'croissant', 'snack',
+                          'chocolate', 'tarta', 'brownie']
+
+
+cluster1_vegano = [probx3_2_1_desayuno, probx3_2_1_comida_cena, probx3_2_1_entre_horas]
+
+# CLUSTER 2
+prob_3_2_2_desayuno = ['cereales', 'pan', 'tostada', 'palitos', 'baguette', 'hogaza', 'baguettes', 'panadería', 'cereales','aceite',
+                       'tostadas','galletas','cerezas','albaricoque', 'manzana', 'plátano', 'fresa','uvas','ciruela', 'higos', 'paraguayo',
+                       'naranja', 'mandarina', 'uva', 'peras', 'pera', 'piña','manzanas', 'manzana', 'banana', 'ciruelas', 'paraguayos', 
+                       'frambuesas', 'aguacates', 'mango', 'aguacate','limón', 'melón', 'sandía', 'kiwis','bífidus','zumo']
+
+prob_3_2_2_comida_cena = ['tortiglioni', 'helice', 'tallarín', 'cannelloni', 'fusilli', 'pasta', 'spaghetti', 'macarrón', 'tallarines',
+                        'garbanzos', 'lentejas', 'alubias', 'cebolla', 'ajo', 'espinaca', 'alcachofa', 'zanahoria', 'albahaca', 'calabaza',
+                        'remolacha', 'salmorejo', 'sopa', 'gazpacho', 'patatas', 'patata', 'arroz', 'tomate', 'helado', 'ensalada', 'champiñones',
+                        'verduras', 'verdura', 'pimientos', 'espinacas', 'haba', 'rollitos', 'rodajas', 'lechuga', 'iceberg', 'zanahoria',
+                        'brócoli', 'berenjena', 'judía', 'berenjenas', 'coliflor', 'pepino', 'escarola', 'tomate', 'puerro', 'calabaza',
+                        'calabacín', 'pimiento', 'patata', 'remolacha', 'zanahoria', 'cebolla', 'ajo', 'ajos', 'puerro', 'maíz', 'remolacha',
+                        'espárrago', 'puerro', 'espinaca', 'rúcula', 'lechuga', 'canónigos', 'coliflor', 'brócoli', 'alcachofa', 'acelgas',
+                        'champiñón', 'seta', 'zanahorias', 'tomates', 'cebollas', 'batata', 'lechugas']
+
+prob_3_2_2_entre_horas =  ['chocolate', 'galletas', 'cheesecake', 'copa', 'brownie', 'cacao', 'choco', 'berlina', 'trenza', 'napolitana', 
+                           'palmeritas', 'mantecadas', 'gofres', 'sobaos', 'croissant', 'snack', 'chocolate', 'pizza', 'tarta', 'mayonesa', 
+                           'nata', 'paté', 'nachos', 'quesos']
+
+cluster2_vegano = [prob_3_2_2_desayuno, prob_3_2_2_comida_cena, prob_3_2_2_entre_horas]
+
+# CLUSTER 3
+prob_3_2_3_desayuno = ['cereales','pan', 'tostada', 'palitos','baguette','hogaza', 'baguettes', 'panadería','cereales','aceite', 'tostadas',
+                        'cerezas','albaricoque','manzana', 'plátano', 'fresa','uvas','ciruela', 'higos', 'paraguayo', 'arándanos',
+                        'nectarina', 'melocotón', 'naranja', 'mandarina', 'uva', 'peras', 'pera', 'piña','manzanas', 'manzana', 'banana',
+                        'ciruelas', 'paraguayos', 'frambuesas', 'aguacates', 'mango', 'aguacate', 'limón', 'melón', 'sandía', 'kiwis',
+                        'bífidus', 'tortitas', 'galletas', 'queso', 'zumo']
+
+prob_3_2_3_comida_cena = ['garbanzo', 'legumbre', 'garbanzos', 'lentejas', 'alubias', 'lenteja', 'alubia', 'espinaca', 'alcachofa', 'zanahoria',
+                        'albahaca', 'calabaza', 'remolacha', 'salmorejo', 'sopa', 'gazpacho', 'patatas', 'patata', 'arroz', 'tomate', 'proteínas',
+                        'integrales', 'ensalada', 'alcachofas', 'champiñones', 'alcachofa', 'verduras', 'verdura', 'pimientos', 'espinacas', 'cebolla',
+                        'haba', 'rollitos', 'rodajas', 'lechuga', 'iceberg', 'zanahoria', 'brócoli', 'berenjena', 'judía', 'berenjenas', 'coliflor',
+                        'pepino', 'escarola', 'alcachofa', 'tomate', 'puerro', 'calabaza', 'calabacín', 'pimiento', 'patata', 'remolacha', 'zanahoria',
+                        'cebolla', 'ajo', 'puerro', 'maíz', 'remolacha', 'espárrago', 'puerro', 'espinaca', 'rúcula', 'lechuga', 'canónigos',
+                        'coliflor', 'brócoli', 'alcachofa', 'acelgas', 'champiñón', 'seta', 'zanahorias', 'tomates', 'cebollas', 'batata', 'lechugas']
+
+prob_3_2_3_entre_horas = []
+
+cluster3_vegano = [prob_3_2_3_desayuno, prob_3_2_3_comida_cena, prob_3_2_3_entre_horas]
+
+# CLUSTER 4
+prob_3_2_4_desayuno = ['cereales','pan', 'tostada', 'palitos','baguette','hogaza','baguettes', 'panadería','cereales','aceite',
+                        'tostadas','muesli','cerezas','albaricoque','manzana', 'plátano', 'fresa','uvas','ciruela',
+                        'higos', 'paraguayo', 'arándanos', 'nectarina', 'melocotón', 'naranja', 'mandarina', 'uva', 'peras',
+                        'pera', 'piña', 'manzanas', 'manzana', 'banana', 'ciruelas', 'paraguayos', 'frambuesas',
+                        'mango', 'limón', 'melón', 'sandía', 'kiwis', 'bífidus', 'galletas', 'tortitas', 'queso', 'zumo']
+
+prob_3_2_4_comida_cena = ['garbanzo', 'legumbre', 'garbanzos', 'lentejas', 'alubias', 'lenteja', 'alubia', 'cebolla', 'ajo', 
+                          'espinaca', 'alcachofa', 'zanahoria', 'albahaca', 'calabaza', 'remolacha', 'salmorejo', 'sopa', 
+                          'gazpacho', 'patatas', 'patata', 'arroz', 'tomate', 'proteínas', 'desnatada', 'desnatada', 'desnatada', 
+                          'avena', 'arroz', '0%', 'integrales', 'avena', 'ensalada', 'alcachofas', 'champiñones', 'alcachofa', 
+                          'verduras', 'verdura', 'pimientos', 'espinacas', 'cebolla', 'haba', 'rollitos', 'rodajas', 'lechuga', 
+                          'iceberg', 'zanahoria', 'brócoli', 'berenjena', 'judía', 'berenjenas', 'coliflor', 'pepino', 'escarola', 
+                          'alcachofa', 'tomate', 'puerro', 'calabaza', 'calabacín', 'pimiento', 'patata', 'remolacha', 'zanahoria', 
+                          'cebolla', 'ajo', 'ajos', 'puerro', 'maíz', 'espárrago', 'puerro', 'espinaca', 'rúcula', 'lechuga', 
+                          'canónigos', 'coliflor', 'brócoli', 'alcachofa', 'acelgas', 'champiñón', 'seta', 'zanahorias', 'tomates', 
+                          'cebollas', 'batata', 'lechugas']
+prob_3_2_4_entre_horas = []
+cluster4_vegano = [prob_3_2_4_desayuno, prob_3_2_4_comida_cena, prob_3_2_4_entre_horas]
---
+++ src/probabilidades_vegetarianos.py
@@ -0,0 +1,81 @@
+probx3_2_1_desayuno = ['cereales', 'pan', 'tostada', 'palitos', 'baguette', 'hogaza', 'baguettes', 'panadería', 'cereales','aceite', 
+                       'tostadas','galletas','tortitas', 'leche', 'cerezas', 'manzana', 'plátano', 'fresa', 'higos', 'paraguayo', 
+                       'nectarina','manzana', 'aguacates', 'mango', 'aguacate',  'limón', 'melón', 'sandía', 'kiwis', 'bífidus', 
+                       'queso', 'yogur', 'mantequilla']
+probx3_2_1_comida_cena = ['huevo','huevos','tortiglioni','helice','tallarín','cannelloni','fusilli','pasta', 'macarrón', 'tallarines', 
+                          'garbanzos', 'lentejas', 'alubias','albóndigas','cebolla', 'ajo','zanahoria', 'albahaca', 'calabaza','salmorejo',
+                          'sopa', 'gazpacho', 'patatas', 'patata', 'arroz', 'tomate','proteínas','arroz','tiramisú','mousse','flan', 
+                          'helado','ensalada','champiñones','pimientos', 'espinacas','cebolla','lechuga', 'iceberg','zanahoria', 'brócoli', 
+                          'berenjena','berenjenas', 'coliflor', 'pepino','alcachofa','tomate', 'puerro', 'calabaza', 'calabacín','pimiento',
+                          'patata', 'remolacha', 'zanahoria', 'cebolla', 'ajo','ajos','puerro','espárrago','canónigos', 'coliflor',
+                          'champiñón', 'seta','zanahorias', 'tomates']
+probx3_2_1_entre_horas = [ 'chocolate', 'galletas','cheesecake', 'copa','brownie','cacao', 'choco', 'berlina', 'gofres', 'sobaos', 
+                          'croissant','snack','chocolate', 'tarta','mayonesa', 'nata', 'nachos','quesos']
+cluster1_vegetariano = [probx3_2_1_desayuno, probx3_2_1_comida_cena, probx3_2_1_entre_horas]
+
+prob_3_2_2_desayuno = ['cereales','pan', 'tostada', 'palitos', 'baguette','hogaza','baguettes', 'panadería','cereales','aceite',  
+                       'tostadas','galletas','leche','cerezas','albaricoque', 'manzana', 'plátano', 'fresa','uvas','ciruela', 
+                       'higos', 'paraguayo','naranja', 'mandarina', 'uva', 'peras', 'pera', 'piña','manzanas', 'manzana', 'banana', 
+                       'ciruelas', 'paraguayos', 'frambuesas', 'aguacates', 'mango', 'aguacate','limón', 'melón', 'sandía', 
+                       'kiwis','bífidus','yogur','cereales', 'pan', 'tostada', 'palitos', 'baguette', 'hogaza', 'baguettes', 
+                       'panadería', 'cereales','aceite',  'tostadas','galletas','tortitas', 'leche', 'cerezas', 'manzana', 'plátano', 
+                       'fresa', 'higos', 'paraguayo', 'nectarina','manzana', 'aguacates', 'mango', 'aguacate',  'limón', 'melón', 
+                       'sandía', 'kiwis', 'bífidus', 'queso', 'yogur', 'mantequilla','zumo','leche']
+prob_3_2_2_comida_cena = ['huevo','huevos','tortiglioni','helice','tallarín','cannelloni','fusilli','pasta', 'spaghetti','macarrón', 
+                          'tallarines','garbanzo','legumbre', 'garbanzos', 'lentejas', 'alubias', 'lenteja', 'alubia', 'cebolla', 
+                          'ajo', 'espinaca', 'alcachofa', 'zanahoria', 'albahaca', 'calabaza', 'remolacha', 'salmorejo','sopa', 
+                          'gazpacho','patatas', 'patata', 'arroz', 'tomate','proteínas', 'arroz','tiramisú','mousse', 'flan', 
+                          'helado','ensalada','alcachofas','champiñones','alcachofa','verduras','verdura', 'pimientos', 'espinacas', 
+                          'cebolla','haba', 'rollitos', 'rodajas', 'lechuga', 'iceberg', 'zanahoria', 'brócoli', 'berenjena','judía',
+                          'berenjenas', 'coliflor', 'pepino', 'escarola', 'alcachofa', 'tomate', 'puerro', 'calabaza', 'calabacín', 
+                          'pimiento', 'patata', 'remolacha', 'zanahoria', 'cebolla', 'ajo','ajos','puerro', 'maíz','remolacha', 
+                          'espárrago', 'puerro', 'espinaca','rúcula', 'lechuga', 'canónigos', 'coliflor', 'brócoli', 'alcachofa',
+                          'acelgas','champiñón', 'seta', 'zanahorias', 'tomates', 'cebollas', 'batata', 'lechugas']
+prob_3_2_2_entre_horas =  ['chocolate', 'galletas','cheesecake', 'copa','brownie','cacao', 'choco', 'berlina', 'trenza', 'napolitana', 
+                           'palmeritas', 'mantecadas','gofres', 'sobaos','croissant','snack','chocolate', 'pizza', 'tarta', 'hamburguesa',
+                           'mayonesa', 'nata','paté','nachos','quesos']
+cluster2_vegetariano = [prob_3_2_2_desayuno, prob_3_2_2_comida_cena, prob_3_2_2_entre_horas]
+
+prob_3_2_3_desayuno = ['cereales','pan', 'tostada', 'palitos','baguette','hogaza', 'baguettes', 'panadería','cereales','aceite',  'tostadas',
+                       'leche','cerezas','albaricoque','manzana', 'plátano', 'fresa','uvas','ciruela', 'higos', 'paraguayo', 'arándanos', 
+                       'nectarina', 'melocotón', 'naranja', 'mandarina', 'uva', 'peras', 'pera', 'piña','manzanas', 'manzana', 'banana', 
+                       'ciruelas', 'paraguayos', 'frambuesas', 'aguacates', 'mango', 'aguacate', 'limón', 'melón', 'sandía', 'kiwis',
+                       'bífidus','yogur','cereales', 'pan', 'tostada', 'palitos', 'baguette', 'hogaza', 'baguettes', 'panadería', 'cereales',
+                       'aceite',  'tostadas','galletas','tortitas', 'leche', 'cerezas', 'manzana', 'plátano', 'fresa', 'higos', 'paraguayo', 
+                       'nectarina','manzana', 'aguacates', 'mango', 'aguacate',  'limón', 'melón', 'sandía', 'kiwis', 'bífidus', 'queso', 
+                       'yogur', 'zumo','leche']
+prob_3_2_3_comida_cena = ['huevo','huevos','tortiglioni','helice','tallarín','cannelloni','fusilli','pasta', 'spaghetti', 'macarrón', 
+                          'tallarines', 'garbanzo','legumbre', 'garbanzos', 'lentejas', 'alubias', 'lenteja', 'alubia','cebolla', 'ajo', 
+                          'espinaca', 'alcachofa', 'zanahoria', 'albahaca', 'calabaza', 'remolacha','salmorejo','sopa', 'gazpacho', 
+                          'patatas', 'patata', 'arroz', 'tomate','proteínas', 'integrales','tiramisú','mousse', 
+                          'flan', 'helado','ensalada','alcachofas','champiñones','alcachofa','verduras','verdura', 'pimientos', 
+                          'espinacas', 'cebolla','haba', 'rollitos', 'rodajas', 'lechuga', 'iceberg','zanahoria', 'brócoli', 
+                          'berenjena','judía','berenjenas', 'coliflor', 'pepino', 'escarola', 'alcachofa', 'tomate', 'puerro', 'calabaza', 
+                          'calabacín','pimiento','patata', 'remolacha', 'zanahoria', 'cebolla', 'ajo','ajos','puerro','maíz','remolacha',
+                          'espárrago', 'puerro','espinaca','rúcula', 'lechuga', 'canónigos', 'coliflor', 'brócoli', 'alcachofa','acelgas',
+                          'champiñón', 'seta','zanahorias', 'tomates', 'cebollas', 'batata', 'lechugas']
+prob_3_2_3_entre_horas = ['quesos']
+cluster3_vegetariano = [prob_3_2_3_desayuno, prob_3_2_3_comida_cena, prob_3_2_3_entre_horas]
+
+prob_3_2_4_desayuno = ['cereales','pan', 'tostada', 'palitos','baguette','hogaza','baguettes', 'panadería','cereales','aceite',  
+                       'tostadas', 'leche','muesli','cerezas','albaricoque','manzana', 'plátano', 'fresa','uvas','ciruela', 
+                       'higos', 'paraguayo', 'arándanos', 'nectarina', 'melocotón', 'naranja', 'mandarina', 'uva', 'peras', 
+                       'pera', 'piña', 'manzanas', 'manzana', 'banana', 'ciruelas', 'paraguayos', 'frambuesas','aguacates', 
+                       'mango', 'aguacate', 'limón', 'melón', 'sandía', 'kiwis','bífidus','yogur','kéfir','cereales', 'pan', 
+                       'tostada', 'palitos', 'baguette', 'hogaza', 'baguettes', 'panadería', 'cereales','aceite',  'tostadas',
+                       'galletas','tortitas', 'leche', 'cerezas', 'manzana', 'plátano', 'fresa', 'higos', 'paraguayo', 
+                       'nectarina','manzana', 'aguacates', 'mango', 'aguacate',  'limón', 'melón', 'sandía', 'kiwis', 
+                       'bífidus', 'queso', 'yogur','zumo','leche']
+prob_3_2_4_comida_cena = ['huevo','huevos','tortiglioni','helice','tallarín','cannelloni','fusilli','pasta', 'spaghetti', 
+                          'macarrón', 'tallarines','garbanzo','legumbre', 'garbanzos', 'lentejas', 'alubias', 'lenteja', 
+                          'alubia','cebolla', 'ajo', 'espinaca', 'alcachofa', 'zanahoria', 'albahaca', 'calabaza', 
+                          'remolacha','salmorejo','sopa', 'gazpacho','patatas', 'patata', 'arroz', 'tomate','proteínas', 'desnatada',  'desnatada', 'desnatada',
+                          'avena','arroz','0%','integrales','avena','ensalada','alcachofas','champiñones','alcachofa',
+                          'verduras','verdura', 'pimientos', 'espinacas', 'cebolla','haba', 'rollitos', 'rodajas', 'lechuga', 
+                          'iceberg','zanahoria', 'brócoli', 'berenjena','judía','berenjenas', 'coliflor', 'pepino', 'escarola', 
+                          'alcachofa','tomate', 'puerro', 'calabaza', 'calabacín','pimiento','patata', 'remolacha', 'zanahoria', 
+                          'cebolla', 'ajo','ajos','puerro','maíz','espárrago','puerro','espinaca','rúcula', 'lechuga', 'canónigos', 
+                          'coliflor', 'brócoli', 'alcachofa','acelgas','champiñón', 'seta', 'zanahorias', 'tomates', 'cebollas', 
+                          'batata', 'lechugas']
+prob_3_2_4_entre_horas = []
+cluster4_vegetariano = [prob_3_2_4_desayuno, prob_3_2_4_comida_cena, prob_3_2_4_entre_horas]
