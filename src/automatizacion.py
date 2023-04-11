2505cd420b8c43e98a9e64e5877d06f06ebc7b06
AitorHernandez1 <64251085+AitorHernandez1@users.noreply.github.com>
2023-04-11 20:48:25 +0200

    Update automatizacion.py

--- src/automatizacion.py
+++ src/automatizacion.py
@@ -61,10 +61,12 @@ import warnings
 warnings.filterwarnings("ignore")
 
 
+
 df2 = pd.DataFrame(wks.get_all_records())
 df = df2
 load_dotenv()
 path = r'../data/cluster_2_1/' # use your path
+ruta_carpeta = os.environ['GITHUB_WORKSPACE'] + 'path'
 #save into df all files
 all_files = glob.glob(path + "/*.xlsx")
 
