1dd0cfc946016c0def09ba80aa5372b2d188f1bd
AitorHernandez1 <64251085+AitorHernandez1@users.noreply.github.com>
2023-07-26 18:20:09 +0200

    Update automatizacion.py

--- src/automatizacion.py
+++ src/automatizacion.py
@@ -158,27 +158,27 @@ for _, _ in df_final.iterrows():
         cesta1 = cesta_pasta[0] + cesta_verdura[0] + cesta_carne[0] + cesta_pescado[0] + cesta_legumbres[0] + cesta_ensaladas[0] + cesta_desayuno[0]
         cesta2 = cesta_pasta[1] + cesta_verdura[1] + cesta_carne[1] + cesta_pescado[1] + cesta_legumbres[1] + cesta_ensaladas[1] + cesta_desayuno[1]
       
-        lista_productos_cambiados = []
+    lista_productos_cambiados = []
 
-        for producto in cesta1:
-            productos_similares = difflib.get_close_matches(producto, mercadona[' Name'])
+    for producto in cesta1:
+        productos_similares = difflib.get_close_matches(producto, mercadona[' Name'])
+        
+        if len(productos_similares) > 0:
+            producto_similar_mas_caro = None
+            precio_original = mercadona[mercadona[' Name'] == producto]['Price'].iloc[0]
             
-            if len(productos_similares) > 0:
-                producto_similar_mas_caro = None
-                precio_original = mercadona[mercadona[' Name'] == producto]['Price'].iloc[0]
-                
-                for prod_similar in productos_similares:
-                    precio_similar = mercadona[mercadona[' Name'] == prod_similar]['Price'].iloc[0]
-                    if precio_similar > precio_original:
-                        if producto_similar_mas_caro is None or precio_similar > mercadona[mercadona[' Name'] == producto_similar_mas_caro]['Price'].iloc[0]:
-                            producto_similar_mas_caro = prod_similar
-                
-                if producto_similar_mas_caro is not None:
-                    lista_productos_cambiados.append(producto_similar_mas_caro)
-                else:
-                    lista_productos_cambiados.append(producto)
+            for prod_similar in productos_similares:
+                precio_similar = mercadona[mercadona[' Name'] == prod_similar]['Price'].iloc[0]
+                if precio_similar > precio_original:
+                    if producto_similar_mas_caro is None or precio_similar > mercadona[mercadona[' Name'] == producto_similar_mas_caro]['Price'].iloc[0]:
+                        producto_similar_mas_caro = prod_similar
+            
+            if producto_similar_mas_caro is not None:
+                lista_productos_cambiados.append(producto_similar_mas_caro)
             else:
                 lista_productos_cambiados.append(producto)
+        else:
+            lista_productos_cambiados.append(producto)
 
     valor = ", ".join(cesta1)
     valor_premium = ", ".join(lista_productos_cambiados)
